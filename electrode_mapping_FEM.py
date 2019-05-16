28ba5bb57fe2f5b2872e579067e11ccec4dbbc96
Torbjørn V Ness <torbness@gmail.com>
2019-05-16 08:22:10 +0200

    Switched from recirpocity, as this does not have as big bounding problem and is intuitively more easy to understand

--- electrode_mapping_FEM.py
+++ electrode_mapping_FEM.py
@@ -8,21 +8,21 @@ import dolfin as df
 
 eps = 1e-9
 
-dx = 300.0
-dy = 5.0
-dz = 5.0
+dx_tunnel = 300.0
+dy_tunnel = 5.0
+dz_tunnel = 5.0
 
-x0 = -dx / 2
-y0 = -dy / 2
+x0 = -dx_tunnel / 2
+y0 = -dy_tunnel / 2
 z0 = 0.0
 
-x1 = x0 + dx
-y1 = y0 + dy
-z1 = z0 + dz
+x1 = x0 + dx_tunnel
+y1 = y0 + dy_tunnel
+z1 = z0 + dz_tunnel
 
-nx = 500
-ny = 20
-nz = 20
+nx = 200
+ny = 10
+nz = 10
 
 sigma = 0.3  # Extracellular conductivity (S/m)
 
@@ -31,7 +31,14 @@ out_folder = 'results'
 sim_name = "tunnel_test"
 fem_fig_folder = "fem_figs"
 [os.makedirs(f, exist_ok=True) for f in [out_folder, fem_fig_folder]]
-#
+
+source_pos = np.load(join(out_folder, "source_pos.npy"))
+imem = np.load(join(out_folder, "axon_imem.npy"))
+num_tsteps = imem.shape[1]
+num_sources = source_pos.shape[0]
+
+
+
 # def analytic_mea(x, y, z):
 #     phi = 0
 #     for idx in range(len(magnitudes)):
@@ -47,8 +54,9 @@ def plot_FEM_results(phi, elec):
     as well as the potential in the center along the z-axis (depth)
     """
 
-    x = np.linspace(x0, x1, 501)
-    z = np.linspace(0, 5, 21)
+    x = np.linspace(x0, x1, nx)
+    z = np.linspace(z0, z1, nz)
+    y = np.linspace(y0, y1, nz)
 
     mea_x_values = np.zeros(len(x))
     # analytic = np.zeros(len(x))
@@ -58,30 +66,54 @@ def plot_FEM_results(phi, elec):
 
     # print(np.max(np.abs(mea_x_values - analytic)))
 
-    phi_plane = np.zeros((len(x), len(z)))
+    phi_plane_xz = np.zeros((len(x), len(z)))
+    phi_plane_xy = np.zeros((len(x), len(z)))
     for x_idx in range(len(x)):
         for z_idx in range(len(z)):
-            phi_plane[x_idx, z_idx] = phi(x[x_idx], 0.0, z[z_idx])
+            phi_plane_xz[x_idx, z_idx] = phi(x[x_idx], 0.0, z[z_idx])
+        for y_idx in range(len(y)):
+            phi_plane_xy[x_idx, y_idx] = phi(x[x_idx], y[y_idx], 0.0 + eps)
+
+
     plt.close("all")
     fig = plt.figure(figsize=[18, 9])
     # fig.suptitle('Charge positions:\n%s' % (str(charge_pos)))
     fig.subplots_adjust(wspace=0.5, bottom=0.25, top=0.85)
     # ax1 = fig.add_subplot(131, aspect=1, ylabel='z [mm]', xlabel='x [mm]', title='Set up')
-    ax2 = fig.add_subplot(211, aspect=1, ylabel='z [mm]', xlabel='x [mm]',
-                          title='Field cross section')
-    ax3 = fig.add_subplot(212, xlabel='x [mm]', ylabel='Field strength', ylim=[-0.6, 0.6])
 
-    img1 = ax2.imshow(phi_plane.T, interpolation='nearest', origin='lower', #cmap='bwr',
-                      extent=(x[0], x[-1], z[0], z[-1]), vmax=0.6, vmin=-0.6)
+    ax_setup = fig.add_subplot(511, aspect=1, xlabel='x [$\mu$m]', ylabel='z [$\mu$m]',
+                          title='Axon and tunnel')
 
-    plt.colorbar(img1, ax=ax2)
+    ax1 = fig.add_subplot(513, aspect=1, xlabel='x [$\mu$m]', ylabel='y [$\mu$m]',
+                          title='Potential cross section')
+
+    ax2 = fig.add_subplot(514, aspect=1, xlabel='x [$\mu$m]', ylabel='z [$\mu$m]', )
+
+    ax3 = fig.add_subplot(515, xlabel='x [$\mu$m]', ylabel='MEA potential (mV)', ylim=[-1.5, 1.5])
+
+    import matplotlib.patches as mpatches
+    rect = mpatches.Rectangle([x0, z0], dx_tunnel, dz_tunnel, ec="k", fc='gray')
+
+    ax_setup.add_patch(rect)
+
+    img1 = ax1.imshow(phi_plane_xy.T, interpolation='nearest', origin='lower', cmap='bwr',
+                      extent=(x[0], x[-1], y[0], y[-1]), vmax=1.5, vmin=-1.5)
+    img2 = ax2.imshow(phi_plane_xz.T, interpolation='nearest', origin='lower', cmap='bwr',
+                      extent=(x[0], x[-1], z[0], z[-1]), vmax=1.5, vmin=-1.5)
+
+    cax = fig.add_axes([0.95, 0.85, 0.01, 0.1])
+
+    plt.colorbar(img1, cax=cax)
     l, = ax3.plot(x, mea_x_values,  lw=2, c='g')
     # l2, = ax3.plot(x, analytic,  lw=2, c='k', ls='--')
 
     plt.savefig(join(fem_fig_folder, 'results_{}_t_idx_{}.png'.format(sim_name, elec)))
 
-def refine_mesh(mesh):
 
+
+
+def refine_mesh(mesh):
+    """" To refine selected parts of the mesh. """
     for r in [2.5]:#[20, 15, 10, 8]:
         print("Refining ...")
         cell_markers = df.MeshFunction("bool", mesh, dim=mesh.topology().dim()-1)
@@ -116,7 +148,7 @@ right = RightTunnel()
 # Define mesh
 mesh = df.BoxMesh(df.Point(x0, y0, z0), df.Point(x1, y1, z1), nx, ny, nz)
 
-print(mesh.num_cells())
+print("Number of cells in mesh: ", mesh.num_cells())
 # mesh = refine_mesh(mesh)
 
 mesh_coordinates = mesh.coordinates()
@@ -124,10 +156,6 @@ mesh_coordinates = mesh.coordinates()
 np.save(join(out_folder, "mesh_coordinates.npy"), mesh_coordinates)
 # np.save(join(out_folder, "electrodes.npy"), electrodes)
 
-source_pos = np.load(join(out_folder, "source_pos.npy"))
-imem = np.load(join(out_folder, "axon_imem.npy"))
-num_tsteps = imem.shape[1]
-num_sources = source_pos.shape[0]
 
 # closest_idxs = np.ones(num_sources, dtype=int)
 # pos_errors = np.zeros(num_sources)
@@ -164,8 +192,8 @@ L = df.Constant(0) * v * dx
 bcs = [df.DirichletBC(V, 0.0, boundaries, 1)]
 
 
-for t_idx in range(num_tsteps):
-# for elec in range(len(electrodes)):
+for t_idx in range(num_tsteps)[64:65]:
+
     print(t_idx)
     phi = df.Function(V)
     A = df.assemble(a)
@@ -181,9 +209,10 @@ for t_idx in range(num_tsteps):
         delta.apply(b)
 
     df.solve(A, phi.vector(), b, 'cg', "ilu")
-    phi_sources = np.zeros(num_sources)
-    # for s_idx, s_pos in enumerate(source_pos):
-    #     phi_sources[s_idx] = phi(s_pos[0], s_pos[1], s_pos[2])
+
+    df.File(join(out_folder, "phi_t_vec_{}.xml".format(t_idx))) << phi
+
+    # phi_sources = np.zeros(num_sources)
 
     # np.save(join(out_folder, "phi_sources_elec_{}.npy".format(elec)), phi_sources)
     np.save(join(out_folder, "phi_t_vec_{}.npy".format(t_idx)), phi.vector())
